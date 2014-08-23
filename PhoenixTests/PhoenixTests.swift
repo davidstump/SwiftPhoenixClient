4ecb5d6195a32a420016eeedb0bf57389fd08012
David Stump <david@davidstump.net>
2014-08-23 14:49:55 -0400

    Initial Commit

---
+++ PhoenixTests/PhoenixTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhoenixTests.swift
+//  PhoenixTests
+//
+//  Created by David Stump on 8/23/14.
+//  Copyright (c) 2014 David Stump. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PhoenixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
