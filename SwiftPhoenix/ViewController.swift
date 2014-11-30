83e427d5574a8ff281e3ab488185b98072a13f99
David Stump <david@davidstump.net>
2014-11-30 12:13:56 -0500

    Initial Commit

---
+++ SwiftPhoenix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPhoenix
+//
+//  Created by David Stump on 11/30/14.
+//  Copyright (c) 2014 David Stump. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
