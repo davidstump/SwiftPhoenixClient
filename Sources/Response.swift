62cdaa7e153e2052df5903bf2ae331c27776c960
Daniel Rees <drees@clustertruck.com>
2018-02-17 21:24:15 -0500

    Changed API to mirror javascript API

--- Sources/Response.swift
+++ Sources/Response.swift
@@ -12,25 +12,25 @@
 import Foundation
 
 /// Maps the data from a Websocket into a Response
-public class Response {
+class Response {
     
     /// The unique string ref
-    public let ref: String
+    let ref: String
     
     /// The string topic or topic:subtopic pair namespace, for example "messages", "messages:123"
-    public let topic: String
+    let topic: String
     
     /// The string event name, for example "phx_join"
-    public let event: String
+    let event: String
     
     /// The message payload
-    public let payload: Socket.Payload
+    let payload: Payload
     
     init?(data: Data) {
         do {
             guard let jsonObject
                 = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions())
-                    as? Socket.Payload
+                    as? Payload
                 else { return nil }
             
             self.ref = jsonObject["ref"] as? String ?? ""
@@ -38,7 +38,7 @@ public class Response {
             if
                 let topic = jsonObject["topic"] as? String,
                 let event = jsonObject["event"] as? String,
-                let payload = jsonObject["payload"] as? Socket.Payload {
+                let payload = jsonObject["payload"] as? Payload {
                 
                 self.topic = topic
                 self.event = event
@@ -51,10 +51,4 @@ public class Response {
             return nil
         }
     }
-    
-    
-    public var description: String {
-        return "ref:\"\(ref)\", event:\"\(event)\", topic:\"\(topic)\", payload:\(payload)"
-    }
-    
 }
