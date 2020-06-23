6df87d94950f20f1f18ccb04fc8a529431c04afa
Andrew Breckenridge <asbreckenridge@me.com>
2020-06-22 22:43:51 -0700

    Specify platforms in Package.swift
    Fails on macOS with an error that `scheduledTimer(withTimeInterval:repeats:block)` is only available after macOS 10.12. I guessed versions for the other platforms, but they seem sensical to me

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftPhoenixClient",
+  platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v2)],
   products: [
     .library(name: "SwiftPhoenixClient", targets: ["SwiftPhoenixClient"])
   ],
