3e46d9c0c85450c5d8c58f629c83139202789c27
dsrees <drees@clustertruck.com>
2020-10-21 21:07:40 -0400

    Running a single test on all platforms

---
+++ Sources/Phoenix/MyClass.swift
@@ -0,0 +1,14 @@
+//
+//  MyClass.swift
+//  SwiftPhoenixClient
+//
+//  Created by Daniel Rees on 10/21/20.
+//  Copyright © 2020 SwiftPhoenixClient. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+  
+  var truthyValue = true
+}
