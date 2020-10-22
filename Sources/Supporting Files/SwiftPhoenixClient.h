291b35d24e78d411b3ce363f93a96cd98f967338
dsrees <drees@clustertruck.com>
2020-10-21 21:00:37 -0400

    Init commit

---
+++ Sources/Supporting Files/SwiftPhoenixClient.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPhoenixClient.h
+//  SwiftPhoenixClient
+//
+//  Created by Daniel Rees on 10/21/20.
+//  Copyright © 2020 SwiftPhoenixClient. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftPhoenixClient.
+FOUNDATION_EXPORT double SwiftPhoenixClientVersionNumber;
+
+//! Project version string for SwiftPhoenixClient.
+FOUNDATION_EXPORT const unsigned char SwiftPhoenixClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPhoenixClient/PublicHeader.h>
+
+
