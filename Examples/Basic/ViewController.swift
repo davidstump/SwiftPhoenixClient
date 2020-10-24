2ff567d21ffc23e9342657524687c6d9167bf3ea
dsrees <drees@clustertruck.com>
2020-10-23 22:03:17 -0400

    Creating NSURL Transport

---
+++ Examples/Basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic
+//
+//  Created by Daniel Rees on 10/23/20.
+//  Copyright © 2020 SwiftPhoenixClient. All rights reserved.
+//
+
+import UIKit
+
+class BasicIntroViewController: UIViewController {
+
+  
+  @IBAction func onGetStartedPressed(_ sender: Any) {
+    
+  }
+  
+}
+
---
+++ Sources/SwiftPhoenixClient/Transport.swift
@@ -0,0 +1,270 @@
+// Copyright (c) 2020 David Stump <david@davidstump.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+
+//----------------------------------------------------------------------
+// MARK: - Transport Protocol
+//----------------------------------------------------------------------
+/**
+ Defines a `Socket`'s Transport layer.
+ */
+// sourcery: AutoMockable
+public protocol Transport {
+  
+  /// The current `ReadyState` of the `Transport` layer
+  var readyState: TransportReadyState { get }
+  
+  /// Delegate for the `Transport` layer
+  var delegate: TransportDelegate? { get set }
+  
+  /**
+   Connect to the server
+   */
+  func connect()
+  
+  /**
+   Disconnect from the server.
+   
+   - Parameters:
+   - code: Status code as defined by <ahref="http://tools.ietf.org/html/rfc6455#section-7.4">Section 7.4 of RFC 6455</a>.
+   - reason: Reason why the connection is closing. Optional.
+   */
+  func disconnect(code: Int, reason: String?)
+  
+  /**
+   Sends a message to the server.
+   
+   - Parameter data: Data to send.
+   */
+  func send(data: Data)
+}
+
+
+//----------------------------------------------------------------------
+// MARK: - Transport Delegate Protocol
+//----------------------------------------------------------------------
+/**
+ Delegate to receive notifications of events that occur in the `Transport` layer
+ */
+public protocol TransportDelegate {
+  
+  /**
+   Notified when the `Transport` opens.
+   */
+  func onOpen()
+  
+  /**
+   Notified when the `Transport` receives an error.
+   
+   - Parameter error: Error from the underlying `Transport` implementation
+   */
+  func onError(error: Error)
+  
+  /**
+   Notified when the `Transport` receives a message from the server.
+   
+   - Parameter message: Message received from the server
+   */
+  func onMessage(message: String)
+  
+  /**
+   Notified when the `Transport` closes.
+   
+   - Parameter code: Code that was sent when the `Transport` closed
+   */
+  func onClose(code: Int)
+}
+
+//----------------------------------------------------------------------
+// MARK: - Transport Ready State Enum
+//----------------------------------------------------------------------
+/**
+ Available `ReadyState`s of a `Transport` layer.
+ */
+public enum TransportReadyState {
+  
+  /// The `Transport` is opening a connection to the server.
+  case connecting
+  
+  /// The `Transport` is connected to the server.
+  case open
+  
+  /// The `Transport` is closing the connection to the server.
+  case closing
+  
+  /// The `Transport` has disconnected from the server.
+  case closed
+  
+}
+
+//----------------------------------------------------------------------
+// MARK: - Default Websocket Transport Implementation
+//----------------------------------------------------------------------
+/**
+ A `Transport` implementation that relies on URLSession's native WebSocket
+ implementation.
+ 
+ This implementation ships default with SwiftPhoenixClient however
+ SwiftPhoenixClient supports earlier OS versions using one of the submodule
+ `Transport` implementations. Or you can create your own implementation using
+ your own WebSocket library or implementation.
+ */
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+public class URLSessionTansport: NSObject, Transport, URLSessionWebSocketDelegate {
+  
+  
+  /// The URL to connect to
+  private let url: URL
+  
+  /// The underling URLsession. Assigned during `connect()`
+  private var session: URLSession? = nil
+  
+  /// The ongoing task. Assigned during `connect()`
+  private var task: URLSessionWebSocketTask? = nil
+  
+  
+  /**
+   Initializes a `Transport` layer built using URLSession's WebSocket
+   
+   Example:
+   
+   ```swift
+   let url = URL("wss://example.com/socket")
+   let transport: Transport = URLSessionTansport(url: url)
+   ```
+   
+   - parameter url: URL to connect to
+   */
+  init(url: URL) {
+    self.url = url
+    
+    super.init()
+  }
+  
+  
+  
+  // MARK: - Transport
+  public var readyState: TransportReadyState = .closed
+  public var delegate: TransportDelegate? = nil
+  
+  public func connect() {
+    // Set the trasport state as connecting
+    self.readyState = .connecting
+    
+    // Create the session and websocket task
+    self.session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+    self.task = self.session?.webSocketTask(with: url)
+    
+    // Start the task
+    self.task?.resume()
+  }
+  
+  public func disconnect(code: Int, reason: String?) {
+    /*
+     TODO:
+     1. Provide a "strict" mode that fails if an invalid error is given
+     2. If strict mode is disabled, default to .invalid
+     3. Provide default .normalClosure function
+     */
+    guard let closeCode = URLSessionWebSocketTask.CloseCode.init(rawValue: code) else {
+      fatalError("Could not create a CloseCode with invalid code: [\(code)].")
+    }
+    
+    self.readyState = .closing
+    self.task?.cancel(with: closeCode, reason: reason?.data(using: .utf8))
+  }
+  
+  public func send(data: Data) {
+    self.task?.send(.data(data)) { (error) in
+      /*
+       TODO: What is the behavior when an error occurs?
+       */
+    }
+  }
+  
+  
+  // MARK: - URLSessionWebSocketDelegate
+  public func urlSession(_ session: URLSession,
+                         webSocketTask: URLSessionWebSocketTask,
+                         didOpenWithProtocol protocol: String?) {
+    print("Web Socket did connect")
+    self.readyState = .open
+    self.delegate?.onOpen()
+    
+    self.ping()
+    // Start receiving messages
+    self.receive()
+  }
+  
+  public func urlSession(_ session: URLSession,
+                         webSocketTask: URLSessionWebSocketTask,
+                         didCloseWith closeCode: URLSessionWebSocketTask.CloseCode,
+                         reason: Data?) {
+    print("Web Socket did disconnect")
+    self.readyState = .closed
+    self.delegate?.onClose(code: closeCode.rawValue)
+  }
+  
+  
+  // MARK: - Private
+  private func receive() {
+    self.task?.receive { result in
+      switch result {
+      case .success(let message):
+        switch message {
+        case .data(let data):
+          print("Data received \(data)")
+        case .string(let text):
+          print("Text received \(text)")
+          self.delegate?.onMessage(message: text)
+        default:
+          fatalError("Unknown result was received. [\(result)]")
+        }
+      case .failure(let error):
+        print("Error when receiving \(error)")
+        self.delegate?.onError(error: error)
+      }
+      
+      // Since `.receive()` is only good for a single message, it must
+      // be called again after a message is received in order to
+      // received the next message.
+      self.receive()
+    }
+  }
+  
+  
+  private func ping() {
+    self.task?.sendPing { error in
+      if let error = error {
+        print("Error when sending PING \(error)")
+      } else {
+        print("Web Socket connection is alive")
+        DispatchQueue.global().asyncAfter(deadline: .now() + 5) {
+          self.ping()
+        }
+      }
+    }
+  }
+  
+  
+  
+}
