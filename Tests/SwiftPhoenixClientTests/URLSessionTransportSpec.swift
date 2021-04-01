4ee3663b3592ccc5ecbf646253bf662d6adc1ad0
dsrees <daniel.rees18@gmail.com>
2021-04-01 12:13:50 -0400

    Manually upgrading to wss  for URLSessionTransport
    Starscream automatically handles upgrading an https url
    to a wss scheme but does not accept when the url has already
    been set to use wss. However URLSession does not do this
    so it is expecting the url to already use wss
    
    Fixed by removing the manual upgrade from the URL builder
    and only upgrading inside of the URLSessionTransport init

---
+++ Tests/SwiftPhoenixClientTests/URLSessionTransportSpec.swift
@@ -0,0 +1,48 @@
+//
+//  URLSessionTransportSpec.swift
+//  SwiftPhoenixClientTests
+//
+//  Created by Daniel Rees on 4/1/21.
+//  Copyright © 2021 SwiftPhoenixClient. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import SwiftPhoenixClient
+
+class URLSessionTransportSpec: QuickSpec {
+  
+  override func spec() {
+    
+    describe("init") {
+      it("replaces http with ws protocols") {
+        if #available(iOS 13, *) {
+          expect(
+            URLSessionTransport(url: URL(string:"http://localhost:4000/socket/websocket")!)
+              .url.absoluteString
+          ).to(equal("ws://localhost:4000/socket/websocket"))
+          
+          expect(
+            URLSessionTransport(url: URL(string:"https://localhost:4000/socket/websocket")!)
+              .url.absoluteString
+          ).to(equal("wss://localhost:4000/socket/websocket"))
+          
+          expect(
+            URLSessionTransport(url: URL(string:"ws://localhost:4000/socket/websocket")!)
+              .url.absoluteString
+          ).to(equal("ws://localhost:4000/socket/websocket"))
+          
+          expect(
+            URLSessionTransport(url: URL(string:"wss://localhost:4000/socket/websocket")!)
+              .url.absoluteString
+          ).to(equal("wss://localhost:4000/socket/websocket"))
+          
+        } else {
+          // Fallback on earlier versions
+          expect("wrong iOS version").to(equal("You must run this test on an iOS 13 device"))
+        }
+      }
+    }
+  }
+}
+
