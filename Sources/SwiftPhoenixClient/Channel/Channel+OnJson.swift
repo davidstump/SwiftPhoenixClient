d2f10de6c5f7077e72fb8c36802b0c8fe3259fb2
dsrees <daniel.rees18@gmail.com>
2025-03-29 23:00:35 -0400

    Split into different files and added updated docs

---
+++ Sources/SwiftPhoenixClient/Channel/Channel+OnJson.swift
@@ -0,0 +1,48 @@
+//
+//  Channel+Callbacks.swift
+//  SwiftPhoenixClient
+//
+//  Created by Daniel Rees on 1/30/25.
+//  Copyright © 2025 SwiftPhoenixClient. All rights reserved.
+//
+
+import Foundation
+
+extension Channel {
+    
+    /// Subscribes on channel events with an `Any` payload.
+    ///
+    /// Example:
+    ///
+    ///     channel.on("event") { [weak self] (message) in
+    ///         print(try! message.payload.get() as! [String: Any])
+    ///         // ["foo": "bar"]
+    ///     }
+    ///
+    /// - parameter event: Event to receive
+    /// - parameter callback: Called with the event's message
+    /// - return: Ref counter of the subscription. See `channel.off(_:, ref:)`.
+    @discardableResult
+    public func on(_ event: String, callback: @escaping (ChannelMessage<Any>) -> Void) -> Int {
+        let ref = bindingRef
+        self.bindingRef = ref + 1
+        
+        let subscription = ChannelSubscription(event: event, ref: ref, callback: callback)
+        self.subscriptions.append(subscription)
+        
+        return subscription.ref
+    }
+    
+    /// Hook into when the Channel is closed.
+    /// Same as `on`, but for the `phx_close` event
+    public func onClose(_ callback: @escaping (ChannelMessage<Any>) -> Void) -> Int {
+        return self.on(ChannelEvent.close, callback: callback)
+    }
+    
+    /// Hook into when the Channel receives an Error.
+    /// Same as `on`, but for the `phx_error` event
+    @discardableResult
+    public func onError(_ callback: @escaping (ChannelMessage<Any>) -> Void) -> Int {
+        return self.on(ChannelEvent.error, callback: callback)
+    }
+}
